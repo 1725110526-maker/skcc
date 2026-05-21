2fface837c9adbce6915d94c094db00a5f9f3c62
1725110526-maker <1725110526@utectulancingo.edu.mx>
2026-05-21 14:18:55 +0000

    CREATED EJERCICIOS

--- ejercicios/semana2/holamundo.py
+++ ejercicios/semana2/holamundo.py
@@ -2,4 +2,32 @@ class NombreClase:
     def __init__(self):
         print("Constructor")
 
+    def metodoUno(self):
+        print("Metodo Uno")
+    
+    def metodoDos(self, numero_uno:int, numero_dos:int)->int:
+        """
+        Este metodo realiza la suma de dos numeros enteros y regresa
+        el resultado
+
+        Args:
+            numero_uno:int - Primero numero para la suma
+            numero_dos:int - Segundo numero para la suma
+
+        Returns:
+            resultado:int - Variable con el resultado de la suma
+        """
+        resultado = numero_uno + numero_dos
+        return resultado
+
+    def metodoTres(self, numero_uno, numero_dos):
+        resultado = numero_uno + numero_dos
+        return resultado
+    
+    def metodoCuatro(self, numero_uno, numero_dos):
+        resultado =  numero_uno + numero_dos
+        print(f"La suma es {resultado}")
+
+
+
 nombre_objeto = NombreClase()
